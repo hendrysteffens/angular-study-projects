042e687869e35b2fc4bf142dd9f979e1ec239d78
hendry.steffens <hendry.steffens@senior.com.br>
2021-04-19 20:08:40 -0300

    First commit

---
+++ basics-assignment-1-start/src/app/app-warning/app-warning.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-warning',
+  templateUrl: './app-warning.component.html',
+  styleUrls: ['./app-warning.component.css']
+})
+export class AppWarningComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
