be03aef726a5ccabcff5570cb923be1567bb840a
hendry.steffens <hendry.steffens@senior.com.br>
2021-04-23 12:16:54 -0300

    First exercise

---
+++ basics-assignment-1-start/src/app/app-show-components/app-show-components.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-show-components',
+  templateUrl: './app-show-components.component.html',
+  styleUrls: ['./app-show-components.component.css']
+})
+export class AppShowComponentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- basics-assignment-1-start/src/app/app.module.ts
+++ basics-assignment-1-start/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppWarningComponent } from './app-warning/app-warning.component';
 import { AppSuccessComponent } from './app-success/app-success.component';
+import { AppShowComponentsComponent } from './app-show-components/app-show-components.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppWarningComponent,
-    AppSuccessComponent
+    AppSuccessComponent,
+    AppShowComponentsComponent
   ],
   imports: [
     BrowserModule,
